d8ce4c029a1388eebba62242b480f7e596706827
jiro4989 <jiroron666@gmail.com>
2020-03-29 19:17:41 +0900

    develコンパイラでもテストするように変更 #20

--- nimbot_server/tests/test_post.nim
+++ nimbot_server/tests/test_post.nim
@@ -5,8 +5,18 @@ const url = "http://localhost:4001/play"
 suite "POST /play":
   setup:
     const prefix = "user_id=test_user&text=" 
-  test "normal case":
+  test "POST stable compiler":
     const code = """c
+
+```
+echo "test"
+```
+"""
+    let encodedText = prefix & code.encodeUrl(true)
+    var client = newHttpClient()
+    echo client.postContent(url, encodedText)
+  test "POST stable devel":
+    const code = """c devel
 
 ```
 echo "test"
